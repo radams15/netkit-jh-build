c95cb3024de17526fc12fb46917340dd545e3664
Rhys Adams <rhys@therhys.co.uk>
2023-03-03 10:47:07 +0000

    Add packaging to workflow.
    Adds script to package to tar.bz2 files, workflow then runs this and creates a release on github.

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUT=dist
+
+rm -rf $OUT
+mkdir -p $OUT
+
+for folder in core fs kernel
+do
+	tar -C $folder/build/ -cjSf $OUT/$folder.tar.bz2 netkit-jh/
+done
+
+sha256sum $OUT/* > $OUT/release.sha256
